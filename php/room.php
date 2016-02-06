14a9b546384c12f0e08ea4f2b1b396f9f4aeebad
amanuel2 <amanuelb43@gmail.com>
2016-02-06 00:12:39 -0500

    deleting stuff

--- php/room.php
+++
@@ -1,9 +0,0 @@
-<php
-$db_host = "AnonymusHost";
-$db_username = "AnonymusUsername";
-$db_password = "AnonymusPassword";
-$db_name = "AnonymusDatabaseName";
-@mysql_connect("$db_host", "$db_username", "$db_password", "$db_name") or die('Could not connect to mySQL Database...');
-@mysql_connect_db("$db_name") or die('No database');
-
-?>
